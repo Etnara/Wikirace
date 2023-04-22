d2411a732e7a0872bbe5362afa9bb8ac931270de
Etnara <zakbutt18@gmail.com>
2023-04-22 18:56:25 -0500

    Remove Search Bar & Maximise Window

--- main.py
+++ main.py
@@ -7,28 +7,42 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Driver Variables
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("start-maximized");
 driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()), options = options)
 
 # Start & Finish Variables
 driver.get("https://en.wikipedia.org/wiki/Special:Random")
 start = driver.current_url
+
 with open("FinishPages.txt", "r", encoding='utf-8') as f:
     finish = f.read().splitlines()
     finish = finish[random.randint(0, len(finish) - 1)]
     finish = "https://en.wikipedia.org/wiki/" + finish
 
-# Goto Start
+# Remove Search Bar
+def removeSearchBar():
+    driver.execute_script("""
+    var i = document.getElementById("searchform");
+    var j = document.getElementById("p-search");
+    if (i != null) i.parentNode.removeChild(i);
+    if (j != null) j.parentNode.removeChild(j);
+    """)
+
+# Start
 driver.get(start)
+removeSearchBar()
+timeStart = time.time()
+
 print("Started at " + driver.current_url.split("https://en.wikipedia.org/wiki/")[1].replace("_", " "))
 print("Get to " + finish.split("https://en.wikipedia.org/wiki/")[1].replace("_", " "))
 
-# Start Timer
-timeStart = time.time()
+
 
 # Loop
 counter = 1
 current = driver.current_url
 while driver.current_url != finish:
+    removeSearchBar()
     if driver.current_url != current:
         counter += 1
         current = driver.current_url
