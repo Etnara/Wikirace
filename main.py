c110b937d4f3cf932f3f4ef4a0760b03e698dcf8
Etnara <zakbutt18@gmail.com>
2023-04-22 18:16:38 -0500

    # of Moves, Time Elapsed, URL Formatting
    Formatted URLs in output
    Display # of moves and time elapsed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,14 +19,30 @@ with open("FinishPages.txt", "r", encoding='utf-8') as f:
 
 # Goto Start
 driver.get(start)
-print("Started at " + driver.current_url + ". Get to " + finish)
-current = driver.current_url
+print("Started at " + driver.current_url.split("https://en.wikipedia.org/wiki/")[1].replace("_", " "))
+print("Get to " + finish.split("https://en.wikipedia.org/wiki/")[1].replace("_", " "))
+
+# Start Timer
+timeStart = time.time()
 
 # Loop
+counter = 1
+current = driver.current_url
 while driver.current_url != finish:
     if driver.current_url != current:
+        counter += 1
         current = driver.current_url
-        print("Changed page to " + driver.current_url)
+        print("Changed page to " + driver.find_element("id", "firstHeading").text)
 
 # Finish
-print("Finished!")
+timeEnd = time.time()
+timeElapsed = int(timeEnd - timeStart)
+
+mins = timeElapsed // 60
+sec = timeElapsed % 60
+if timeElapsed > 3600:
+    print("Really bruh. Over an hour?!?!")
+elif mins != 0:
+    print("Finished in " + str(counter) + " moves and " + str(mins) + " minutes and " + str(sec) + " seconds!")
+else:
+    print("Finished in " + str(counter) + " moves and " + str(sec) + " seconds!")
